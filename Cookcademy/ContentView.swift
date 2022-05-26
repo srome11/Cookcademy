9e60311346c3fd396a2b96313be30a26e6a91cac
srome <srome11@gmail.com>
2022-05-26 10:07:13 -0500

    Initial Commit

---
+++ Cookcademy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cookcademy
+//
+//  Created by Scott Rome on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Scott Rome on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
